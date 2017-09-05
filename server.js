c1aa17ea3d53d987b2c9c3ac1cff1beee50dc04f
Dean <dlyoung119@me.com>
2017-09-04 19:24:10 -0700

    updated to send on every vehicle stop

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use(bodyParser.json());
 var client = redis.createClient(process.env.REDIS_URL);
 
 var lastFuelReading = 100.0;
-var notificationSent = false;
 
 app.get('/', function(req, res) {
     client.get('lastFuelReading', function(err, lastFuelReading) {
@@ -48,105 +47,49 @@ app.get('/', function(req, res) {
 });
 
 app.post('/webhook', function(req, res) {
-    var payload = req.body;
-
-    console.log('Webhook received of type \'' + payload.type + '\'')
-
-    if (payload.type == 'ignition:off' ||
-        payload.type == 'trip:finished') {
-        console.log('Checking remaining fuel in vehicle');
-
-        request.get({
-            uri: 'https://api.automatic.com/vehicle/' + payload.vehicle.id + '/',
-            headers: {
-                Authorization: 'Bearer ' + process.env.AUTOMATIC_ACCESS_TOKEN
-            },
-            json: true
-        }, function(error, response, body) {
-            if (body.fuel_level_percent <= process.env.FUEL_PERCENT_THRESHOLD) {
-                console.log('Fuel level at ' + body.fuel_level_percent + '%, below threshold');
-
-                client.get('lastFuelReading', function(err, lastFuelReading) {
-                    if (lastFuelReading == null) {
-                        console.log ('Unable to retrieve lastFuelReading.');
-                        lastFuelReading = 100.0;
-                    }
-
-                    client.get('notificationSent', function(err, notificationSent) {
-                        if (notificationSent == null) {
-                            console.log ('Unable to retrieve notificationSent.');
-                            notificationSent = false;
-                        }
-                        
-                        notificationSent = (notificationSent == 'true');
-                        
-                        console.log('lastFuelReading = ' + lastFuelReading);
-                        console.log('notificationSent = ' + notificationSent);
-
-                        if (body.fuel_level_percent > lastFuelReading &&
-                            body.fuel_level_percent > process.env.FUEL_PERCENT_THRESHOLD) {
-                            console.log('Vehicle has been refuelled, resetting notification flag');
-                            notificationSent = false;
-                            client.set('notificationSent', false);
-                        }
-
-                        if (notificationSent == false) {
-                            console.log('Sending IFTTT event to Maker channel');
-
-                            request.post('https://maker.ifttt.com/trigger/automatic-ifttt/with/key/' + process.env.IFTTT_SECRET_KEY, {
-                                form: {
-                                    value1: body.fuel_level_percent,
-                                    value2: payload.location.lat,
-                                    value3: payload.location.lon
-                                }
-                            }, function(err, response, body) {
-                                console.log('Succeeded');
-                                
-                                notificationSent = true;
-                                client.set('notificationSent', true);
-                            });
-                        } else {
-                            console.log('Notification has already been sent');
-                        }
-                    });
-                });
-            } else {
-                console.log('Fuel level at ' + body.fuel_level_percent + '%, above threshold');
-                
-                client.get('lastFuelReading', function(err, lastFuelReading) {
-                    if (lastFuelReading == null) {
-                        console.log ('Unable to retrieve lastFuelReading.');
-                        lastFuelReading = 100.0;
-                    }
-
-                    client.get('notificationSent', function(err, notificationSent) {
-                        if (notificationSent == null) {
-                            console.log ('Unable to retrieve notificationSent.');
-                            notificationSent = false;
-                        }
-                        
-                        notificationSent = (notificationSent == 'true');
-                        
-                        console.log('lastFuelReading = ' + lastFuelReading);
-                        console.log('notificationSent = ' + notificationSent);
-                        
-                        if (body.fuel_level_percent > lastFuelReading &&
-                            body.fuel_level_percent > process.env.FUEL_PERCENT_THRESHOLD) {
-                            console.log('Vehicle has been refuelled, resetting notification flag');
-                            notificationSent = false;
-                            client.set('notificationSent', false);
-                        }
-                    });
-                });
-            }
-
-            client.set('lastFuelReading', body.fuel_level_percent);
-        });
-    } else {
-        console.log('Ignored');
-    }
-
-    res.status(200).end();
+	
+	var payload = req.body;
+	
+	console.log('Webhook received of type \'' + payload.type + '\'')
+	
+	if (payload.type == 'ignition:off' || payload.type == 'trip:finished') {
+		
+		console.log('Checking remaining fuel in vehicle');
+		
+		request.get({
+			
+			uri: 'https://api.automatic.com/vehicle/' + payload.vehicle.id + '/',
+			headers: {
+				Authorization: 'Bearer ' + process.env.AUTOMATIC_ACCESS_TOKEN
+			},
+			json: true
+			
+		}, function(error, response, body) {
+			
+			console.log('Fuel level at ' + body.fuel_level_percent + '%, above threshold');
+			
+			client.get('lastFuelReading', function(err, lastFuelReading) {
+				
+				if (lastFuelReading == null) {
+					
+					console.log ('Unable to retrieve lastFuelReading.');
+					lastFuelReading = 100.0;
+					
+			}
+			
+			console.log('lastFuelReading = ' + lastFuelReading);
+			
+		});
+		
+		client.set('lastFuelReading', body.fuel_level_percent);
+		
+	} else {
+		
+		console.log('Ignored');
+		
+	}
+	
+	res.status(200).end();
 });
 
 app.listen(process.env.PORT || 3000);
