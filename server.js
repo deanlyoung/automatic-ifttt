60cf5e0799303851031c56621bf92882b788ed31
Dean Young <dlyoung119@me.com>
2018-01-21 21:21:24 -0800

    oath login test

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-var express = require('express');
+const express = require('express');
+const session = require('express-session');
 var bodyParser = require('body-parser');
 var request = require('request');
 var redis = require('redis');
-
-var app = express();
+const port = process.env.PORT || 3000;
+const app = express();
 
 app.use(bodyParser.json());
 
@@ -12,7 +13,72 @@ var client = redis.createClient(process.env.REDIS_URL);
 var lastFuelReading = 100.0;
 var lastTripId = 'T';
 
+// Add your automatic client id and client secret here or as environment variables
+const AUTOMATIC_CLIENT_ID = process.env.AUTOMATIC_CLIENT_ID || 'your-automatic-client-id';
+const AUTOMATIC_CLIENT_SECRET = process.env.AUTOMATIC_CLIENT_SECRET || 'your-automatic-client-secret';
+
+const oauth2 = require('simple-oauth2')({
+  clientID: AUTOMATIC_CLIENT_ID,
+  clientSecret: AUTOMATIC_CLIENT_SECRET,
+  site: 'https://accounts.automatic.com',
+  tokenPath: '/oauth/access_token'
+});
+
+// Authorization uri definition
+const authorizationUri = oauth2.authCode.authorizeURL({
+  scope: 'scope:user:profile scope:trip scope:location scope:vehicle:profile scope:vehicle:events scope:behavior'
+});
+
+// Enable sessions
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
+
+// Initial page redirecting to Automatic's oAuth page
+app.get('/auth', (req, res) => {
+  res.redirect(authorizationUri);
+});
+
+// Callback service parsing the authorization token and asking for the access token
+app.get('/redirect', (req, res) => {
+  const code = req.query.code;
+
+  function saveToken(error, result) {
+    if (error) {
+      console.log('Access token error', error.message);
+      res.send('Access token error: ' +  error.message);
+      return;
+    }
+
+    // Attach `token` to the user's session for later use
+    // This is where you could save the `token` to a database for later use
+    req.session.token = oauth2.accessToken.create(result);
+
+    res.redirect('/welcome');
+  }
+
+  oauth2.authCode.getToken({
+    code: code
+  }, saveToken);
+});
+
+app.get('/welcome', (req, res) => {
+  if (req.session.token) {
+    // Display token to authenticated user
+    console.log('Automatic access token', req.session.token.token.access_token);
+    res.send('You are logged in.<br>Access Token: ' +  req.session.token.token.access_token);
+  } else {
+    // No token, so redirect to login
+    res.redirect('/');
+  }
+});
+
 app.get('/', function(req, res) {
+	
+	res.send('<a href="/auth">Log in with Automatic</a>');
+	
 	client.get('lastFuelReading', function(err, lastFuelReading) {
 		if (lastFuelReading == null) {
 			console.log ('Unable to retrieve lastFuelReading, setting to 100%');
